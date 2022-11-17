6be9aa69ca4de417c79e2028cf4721618bddef19
Spartan <alik_karibov@mail.ru>
2022-11-17 12:57:14 +0400

    123

--- src/main/java/com/example/springboot3/controller/UserController.java
+++ src/main/java/com/example/springboot3/controller/UserController.java
@@ -1,41 +1,44 @@
 package com.example.springboot3.controller;
 
+
+import com.example.springboot3.entity.Role;
+import com.example.springboot3.entity.User;
 import com.example.springboot3.service.RoleService;
-import com.example.springboot3.service.RoleServiceImpl;
-import com.example.springboot3.service.UserService;
 import com.example.springboot3.service.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import com.example.springboot3.entity.Role;
-import com.example.springboot3.entity.User;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
-@Controller
-@RequestMapping(value = "/")
-public class UserController {
-    private final UserService userService;
-    private final RoleService roleService;
 
-    public UserController(UserService userService, RoleService roleService) {
+@Controller
+public class UserController {
+
+    private UserServiceImpl userService;
+    private RoleService roleService;
+
+    @Autowired
+    public UserController(UserServiceImpl userService, RoleService roleService) {
         this.userService = userService;
         this.roleService = roleService;
     }
 
     @GetMapping(value = "/user")
-    public String userInfo(@AuthenticationPrincipal User user, Model model) {
+    public String userInfo(@AuthenticationPrincipal User user, Model model){
         model.addAttribute("user", user);
         model.addAttribute("roles", user.getRoles());
         return "userpage";
     }
 
     @GetMapping(value = "/admin")
-    public String listUsers(Model model) {
+    public String listUsers(@AuthenticationPrincipal User user, Model model) {
         model.addAttribute("allUsers", userService.getAllUsers());
+        model.addAttribute("roles", roleService.getAllRoles());
+        model.addAttribute("user", user);
         return "all-user";
     }
 
@@ -63,6 +66,7 @@ public class UserController {
         model.addAttribute("roles", roleService.getAllRoles());
         return "edit";
     }
+
     @PatchMapping(value = "/{id}")
     public String editUser(@ModelAttribute User user, @RequestParam(value = "checkBoxRoles") String[] checkBoxRoles) {
         Set<Role> roleSet = new HashSet<>();
@@ -74,14 +78,14 @@ public class UserController {
         return "redirect:/admin";
     }
 
-    @DeleteMapping(value = "/remove/{id}")
+    @PostMapping(value = "/remove/{id}")
     public String removeUser(@PathVariable("id") long id) {
         userService.removeUserById(id);
         return "redirect:/admin";
     }
 
-    @GetMapping(value = "/login")
-    public String login() {
+    @GetMapping(value ="/login")
+    public String login(){
         return "login";
     }
 }
